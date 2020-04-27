31054c1bbe262c32692c940a539c1c7660ee2708
NeroBeats <Hani.Hamshari@Hotmail.com>
2020-04-27 13:57:30 +0300

    requirements

---
+++ capstone_env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\dell\desktop\development\django_rest\capstone-backend\capstone_env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
